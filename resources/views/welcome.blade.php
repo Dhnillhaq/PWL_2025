b8317d2e4338d1d1aeb52618587c0fe00ce9fe7e
Dhnillhaq <yakulhaq@gmail.com>
2025-02-18 10:52:05 +0700

    feat : praktikum 1 week 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -142,6 +142,6 @@
 </head>
 <body>
     <h2>Pemrograman Web  Lanjut 2024</h2>
-    <h3>Semangat Belajar Laravel 10</h3>
+    <h3>Semangat Belajar Laravel 10 <?= $name ?></h3>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return 'NIM : 2341720133 <br> NAMA : Muhammad Dhia Ramadhanil Haq';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/user/{name?}', function ($name="John") {
+    return 'Nama saya ' . $name;
+});
+
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/profile', function () {
+    //
+    })->name('profile');
+
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
